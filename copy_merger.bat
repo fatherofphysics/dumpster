261d204ed4305ef238b506687ea920d356e1819c
Vedant Kanoujia <vedantkanoujia@gmail.com>
2023-11-27 07:20:22 +0530

    Copy  &  merge excel files

---
+++ copy_merger.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+title Copy_merge Excel
+echo Copy and Merge Excel formats
+echo Press 1 to merge xls
+echo Press 2 to merge csv
+set /p key=
+if %key% == 1 goto xls
+if %key% == 2 goto csv
+:xls
+copy *.xls merge.xls
+pause
+exit
+:csv
+copy *.csv merge.csv
+pause
+exit

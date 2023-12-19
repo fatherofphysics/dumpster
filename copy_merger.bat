298de12cb8c73fd57d792f7143158d7a98bd3d26
Vedant Kanoujia <vedantkanoujia@gmail.com>
2023-12-19 15:50:19 +0530

    Update copy_merger.bat

--- copy_merger.bat
+++ copy_merger.bat
@@ -4,9 +4,11 @@ title Copy_merge Excel
 echo Copy and Merge Excel formats
 echo Press 1 to merge xls
 echo Press 2 to merge csv
+echo Press 3 to merge xlsx
 set /p key=
 if %key% == 1 goto xls
 if %key% == 2 goto csv
+if %key% == 3 goto xlsx
 :xls
 copy *.xls merge.xls
 echo press any key to close
@@ -15,5 +17,10 @@ exit
 :csv
 copy *.csv merge.csv
 echo press any key to close
+pause>nul
+exit
+:xlsx
+robocopy . merge xlsx
+echo Press any key to close
 pause>nul
 exit
